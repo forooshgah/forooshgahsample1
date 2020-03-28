79428fc170a3c4e7efabb07396fd75ba40c1e1ef
Ali <Ali@DESKTOP-9RPR8EH>
2020-03-28 17:59:56 +0430

    pagination-1

--- src/app/pages/products/productList.js
+++ src/app/pages/products/productList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { LinearProgress, Grid } from '@material-ui/core';
-import { Alert } from 'react-bootstrap';
+import { Alert, Pagination } from 'react-bootstrap';
 import { searchProduct } from '../../services/product.service'
 import ProductTile from './ProductTile'
 
@@ -26,13 +26,38 @@ const ProductList = (props) => {
         list = <Alert severity="warning">Nothing found!</Alert>
     } else {
 
-        let prodInTiles = products.map(prod => 
-                <Grid item xs={12} sm={6} md={4} xl={3} key={prod.id}>
-                    <ProductTile product={prod} />
-                </Grid>
+        let prodInTiles = products.map(prod =>
+            <Grid item xs={12} sm={6} md={4} xl={3} key={prod.id}>
+                <ProductTile product={prod} />
+            </Grid>
         );
 
-        list = (<Grid container spacing={3}>{prodInTiles}</Grid>);
+
+        let paganation = (
+            <Pagination>
+                <Pagination.First />
+                <Pagination.Prev />
+                <Pagination.Item>{1}</Pagination.Item>
+                <Pagination.Ellipsis />
+
+                <Pagination.Item>{10}</Pagination.Item>
+                <Pagination.Item>{11}</Pagination.Item>
+                <Pagination.Item active>{12}</Pagination.Item>
+                <Pagination.Item>{13}</Pagination.Item>
+                <Pagination.Item disabled>{14}</Pagination.Item>
+
+                <Pagination.Ellipsis />
+                <Pagination.Item>{20}</Pagination.Item>
+                <Pagination.Next />
+                <Pagination.Last />
+            </Pagination>
+        );
+
+        list = (
+            <Grid container spacing={3}>{prodInTiles}
+                <Grid item xs={12}>{paganation}</Grid>
+            </Grid>);
+
     }
 
 
